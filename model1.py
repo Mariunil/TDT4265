2c36e865d157c7181be37a1ecf26d05a2bff6db2
marius <nilsen-marius@hotmal.com>
2019-02-25 09:27:49 +0100

    a

--- model1.py
+++ model1.py
@@ -89,14 +89,14 @@ class Trainer:
         self.epochs = 100
         self.batch_size = 64
         self.learning_rate = 5e-2
-        self.momentum = 0.9
+        self.momentum = 0.2
         self.L2 = 0.001
         self.nesterov = False
         self.early_stop_count = 4
         self.should_anneal = True
         self.T = 5
         self.t = 0
-        self.a0 = 1e-1
+        self.a0 = 9e-1
 
         # Architecture
 
