3e863e9f5f0999c640d76c6bbcfb4186f8d6d495
marius <nilsen-marius@hotmal.com>
2019-02-28 09:12:41 +0100

    s

--- tester.py
+++ tester.py
@@ -13,19 +13,19 @@ if __name__ == "__main__":
     trainer.train()
 
     file1 = open("model3_test_acc.txt", "w")
-    file1.write(model3.trainer.TEST_ACC)
+    file1.write(trainer.TEST_ACC)
 
     file2 = open("model3_val_acc.txt", "w")
-    file2.write(model3.trainer.VALIDATION_ACC)
+    file2.write(trainer.VALIDATION_ACC)
 
     file3 = open("model3_train_acc.txt", "w")
-    file3.write(model3.trainer.TRAIN_ACC)
+    file3.write(trainer.TRAIN_ACC)
 
     file4 = open("model3_test_loss.txt", "w")
-    file4.write(model3.trainer.TEST_LOSS)
+    file4.write(trainer.TEST_LOSS)
 
     file5 = open("model3_train_loss.txt", "w")
-    file5.write(model3.trainer.TRAIN_LOSS)
+    file5.write(trainer.TRAIN_LOSS)
 
     file6 = open("model3_val_loss.txt", "w")
-    file6.write(model3.trainer.VALDIATION_LOSS)
+    file6.write(trainer.VALDIATION_LOSS)
